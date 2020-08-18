2b42472d009a0fd93976604a01a9f39f3129b3df
cacauisadog <cbcaue@gmail.com>
2020-08-17 22:59:54 -0300

    add dev.sh with dkdb function

---
+++ dev.sh
@@ -0,0 +1,13 @@
+PROJ_BASE="$(dirname "${BASH_SOURCE[0]}")"
+CD=$(pwd)
+cd "$PROJ_BASE" || exit
+PROJ_BASE=$(pwd)
+export PROJ_BASE
+cd "$CD" || exit
+
+function dkdb {
+  docker run -e POSTGRES_USER=mentira -e POSTGRES_PASSWORD=mentira \
+    -p 5432:5432 \
+    -v "$PROJ_BASE"/dkdata/postgres-mentira:/var/lib/postgresql/data \
+    postgres:12
+}
