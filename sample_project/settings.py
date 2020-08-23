4112400e5f15f7e25efaf87d009027bf6f846a36
cacauisadog <cbcaue@gmail.com>
2020-08-23 15:42:22 -0300

    add db logging on debug

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -104,6 +104,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
