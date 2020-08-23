d13a44e62c3c88387da0e2d341639b875f0cb7fd
cacauisadog <cbcaue@gmail.com>
2020-08-23 18:05:42 -0300

    add tests

--- mentirinha/tests.py
+++ mentirinha/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from mentirinha.models import ShortenedUrl
+
+
+class TestMentirinha(TestCase):
+    fixtures = ["urls.json", ]
+
+    def setUp(self):
+        self.url = ShortenedUrl.objects.get(pk=1)
+        self.client = Client()
+
+    def test_existing_short_code_should_redirect_with_302(self):
+        short_code = self.url.short_code
+        r = self.client.get(f'/{short_code}')
+        self.assertEqual(302, r.status_code)
+
+    def test_inexisting_short_code_should_respond_with_404(self):
+        short_code = 'idontexist'
+        r = self.client.get(f'/{short_code}')
+        self.assertEqual(404, r.status_code)
